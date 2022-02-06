12324f1adc6d0ac30062bf9c597ca59dce449219
William Okano <williamokano@gmail.com>
2022-02-06 08:44:14 +0100

    added reminder part

--- src/App.js
+++ src/App.js
@@ -31,10 +31,14 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id))
   }
 
+  const toggleReminder = id => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : ('No Tasks to Show')}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks to Show')}
     </div>
   );
 }
